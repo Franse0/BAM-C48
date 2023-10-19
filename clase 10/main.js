b7e9335ef3a2fa3d68c9a184b5b7e164a1b73c29
Franse0 <panchymarin.fn@gmail.com>
2023-10-19 12:57:36 -0300

    clase 10

---
+++ clase 10/main.js
@@ -0,0 +1,111 @@
+// Console.log se usa para imprimir por consola
+// console.log("Hola, estamos en la clase 10");
+// Con let no podemos definir una variable con el mismo nombre a una ya creada
+// let a = "hola";
+// let a = 2;
+
+// Var nos permite declara una variable con el mismo nombre a una variable ya creada
+// var a = "hola";
+// var a = 2;
+
+// Alert
+// alert("Soy un alert")
+
+// prompt
+// prompt("Como te llamas?")
+
+// let a = true;
+// let b = true;
+
+// console.log(a && b) devuelve true;
+// console.log(2<3 && a && b);
+
+
+// Operador or quiere decir si una u otra condicion se cumple
+// let a = true;
+// let b = true;
+// console.log(a || 1>2);
+
+
+// Not !
+// console.log(!false);
+
+
+// Mas de un operador 
+// let a = true;
+// let b = true;
+// console.log((a && b)||(1<10 && !a));
+// console.log(true || false);
+
+/* operado ==
+console.log(2 == 3);
+
+operador estrictamente igual ===
+console.log(2==="2");
+
+operador != diferente 
+console.log(2!=3);
+
+operador estrictamente dif !==
+console.log(3!==3);
+
+
+console.log(5>4);
+console.log(5<4);
+console.log(5>=5);
+console.log(5<=10);
+
+*/
+
+
+// Operadores Matematicos
+// let a = 5;
+// a = a +2;
+// a +=2;
+
+// let a= 2;
+// a -=1 ;
+// a = a -1;
+// a -=5 ;
+// a *=2;
+// a /=2;
+
+// console.log(a);
+
+// let bateria = false;
+
+// if(bateria===true){
+//     console.log("true")
+// } else {
+//     console.log("celular enchufado")
+// }
+
+// const persona = 18;
+// const nombre = "Fran";
+
+// if(persona>=18){
+//     console.log("que se tome una birrita");
+// } else if(persona>=15) {
+//     console.log("que salga a una matine");
+// } else{
+//     console.log("Que espere");
+// };
+// const string = 112;
+// Un array se define con corchetes []
+// const arr = ["hola", "hola2",2,true, "Hola3",4,5,6,["almacena","mas","datos"]];
+// console.log(arr);18
+
+// var edad = prompt("Cuale es tu nombre");
+// if(edad>=18){
+//     alert("que pase");
+// } else {
+//     alert(`que todavia no pase por que tiene ${edad} años`)
+// }
+
+
+const nombre = "Catalina";
+const apellido = "Iglesias";
+
+console.log(`Hola soy ${nombre} ${apellido}`);
+
+
